c90aac6a8a6fb89c7c8e03fb577d1b41f9f815f9
hapeace <danangpostman37@gmail.com>
2022-07-08 15:47:35 +0700

    resolving again

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import React from "react";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="px-36 pt-20">{children}</main>
+      <main className="px-36 pt-20">{React.Children.only(children)}</main>
       {/* <Footer /> */}
     </>
   );
